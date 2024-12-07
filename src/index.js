816a4783143935f0debac0a63c8c70be4516cbf4
Dheeraj Verma <vermadheeraj945@gmail.com>
2024-12-07 21:32:28 +0530

    added cors options

--- src/index.js
+++ src/index.js
@@ -9,12 +9,15 @@ import {authRoutes,messageRoutes,groupChatRoutes,groupRoutes,groupReqRoutes,user
 
 dotenv.config();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+const corsOptions = {
+  origin: ["http://localhost:5173", "https://chit-chat-peach.vercel.app"],
+  credentials: true,
+  methods: ["GET", "POST","PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
